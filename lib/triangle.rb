ab8909af8eb4e238217758b85864b4cf7c9de826
Shane Geary <stg4462@comcast.net>
2020-02-10 15:02:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       raise TriangleError
     end 
   end 
-  if (@side1 + @side2) <= @side3 ||(@side2 + @side3) <= @side1 || (@side + @side3) <=
+  if (@side1 + @side2) <= @side3 ||(@side2 + @side3) <= @side1 || (@side1 + @side3) <= @side2 
   if (@side1 == @side2 && @side2 == @side3)
     return :equilateral
     elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
