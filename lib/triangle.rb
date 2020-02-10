38ab7b43f0a87cd17a47077c5cdec3a699de04be
Shane Geary <stg4462@comcast.net>
2020-02-10 14:50:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,5 +16,6 @@ class Triangle
   if (@side1 == @side2 && @side2 == @side3)
     return :equilateral
     elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
-    return :
+    return :isosceles
+    
 end
